c7ea0088ea0b5b116209440f0372f507d710c64a
Joshua Reibert <joshua.reibert@uni-weimar.de>
2014-04-08 17:52:35 +0200

    add small powerwall, add warpmatrices to display config

--- configs/display_config.py
+++ configs/display_config.py
@@ -1,20 +1,87 @@
 #!/usr/bin/python
 
 display_config = [
-    {
+  {
+  ##################################################
       "name":             "large_powerwall"
+  ##################################################
     , "hostname":         "kerberos"
     , "displaystrings":   [":0.0", ":0.1", ":0.2", ":0.3"]
     , "resolution":       (1920, 1200)
     , "size":             (4.16, 2.6)
     , "transform":        (0.0, 1.57, 0.0)
-    , "warpmatricespath": "/opt/dlp-warpmatrices/"
-    }
+    , "warpmatrices":     [
+      (
+          "/opt/dlp-warpmatrices/dlp_6_warp_P4.warp"
+        , "/opt/dlp-warpmatrices/dlp_6_warp_P5.warp"
+        , "/opt/dlp-warpmatrices/dlp_6_warp_P6.warp"
+        , "/opt/dlp-warpmatrices/dlp_6_warp_P1.warp"
+        , "/opt/dlp-warpmatrices/dlp_6_warp_P2.warp"
+        , "/opt/dlp-warpmatrices/dlp_6_warp_P3.warp"
+      )
+      , (
+          "/opt/dlp-warpmatrices/dlp_6_warp_P4.warp"
+        , "/opt/dlp-warpmatrices/dlp_6_warp_P5.warp"
+        , "/opt/dlp-warpmatrices/dlp_6_warp_P6.warp"
+        , "/opt/dlp-warpmatrices/dlp_6_warp_P1.warp"
+        , "/opt/dlp-warpmatrices/dlp_6_warp_P2.warp"
+        , "/opt/dlp-warpmatrices/dlp_6_warp_P3.warp"
+      )
+      , (
+          "/opt/dlp-warpmatrices/dlp_6_warp_P4.warp"
+        , "/opt/dlp-warpmatrices/dlp_6_warp_P5.warp"
+        , "/opt/dlp-warpmatrices/dlp_6_warp_P6.warp"
+        , "/opt/dlp-warpmatrices/dlp_6_warp_P1.warp"
+        , "/opt/dlp-warpmatrices/dlp_6_warp_P2.warp"
+        , "/opt/dlp-warpmatrices/dlp_6_warp_P3.warp"
+      )
+      , (
+          "/opt/dlp-warpmatrices/dlp_6_warp_P4.warp"
+        , "/opt/dlp-warpmatrices/dlp_6_warp_P5.warp"
+        , "/opt/dlp-warpmatrices/dlp_6_warp_P6.warp"
+        , "/opt/dlp-warpmatrices/dlp_6_warp_P1.warp"
+        , "/opt/dlp-warpmatrices/dlp_6_warp_P2.warp"
+        , "/opt/dlp-warpmatrices/dlp_6_warp_P3.warp"
+      )
+    ]
+  }
+
   , {
+  ##################################################
+      "name":             "small_powerwall"
+  ##################################################
+    , "hostname":         "tba"
+    , "displaystrings":   [":0.0", ":0.1"]
+    , "resolution":       (1920, 1200)
+    , "size":             (3.0, 1.98)
+    , "transform":        (0.0, 1.42, 0.0)
+    , "warpmatrices":     [
+      (
+          "/opt/lcd-warpmatrices/lcd_4_warp_P2.warp"
+        , "/opt/lcd-warpmatrices/lcd_4_warp_P2.warp"
+        , "/opt/lcd-warpmatrices/lcd_4_warp_P2.warp"
+        , "/opt/lcd-warpmatrices/lcd_4_warp_P1.warp"
+        , "/opt/lcd-warpmatrices/lcd_4_warp_P1.warp"
+        , "/opt/lcd-warpmatrices/lcd_4_warp_P1.warp"
+      )
+      , (
+          "/opt/lcd-warpmatrices/lcd_4_warp_P4.warp"
+        , "/opt/lcd-warpmatrices/lcd_4_warp_P4.warp"
+        , "/opt/lcd-warpmatrices/lcd_4_warp_P4.warp"
+        , "/opt/lcd-warpmatrices/lcd_4_warp_P3.warp"
+        , "/opt/lcd-warpmatrices/lcd_4_warp_P3.warp"
+        , "/opt/lcd-warpmatrices/lcd_4_warp_P3.warp"
+      )
+    ]
+  }
+
+  , {
+  ##################################################
       "name":             "atalante_display"
+  ##################################################
     , "hostname":         "atalante"
     , "resolution":       (1920, 1080)
     , "size":             (0.40, 0.20)
     , "transform":        (0.0, 1.0, 0.0)
-    }
+  }
 ]

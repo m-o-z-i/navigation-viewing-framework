2c4a41cd8af1b61b029e3de4722cb2e96f610dff
Joshua Reibert <joshua.reibert@uni-weimar.de>
2014-04-09 11:31:22 +0200

    change display config to use classes instead of dicts

--- configs/display_config.py
+++ configs/display_config.py
@@ -1,87 +1,63 @@
 #!/usr/bin/python
 
-display_config = [
-  {
-  ##################################################
-      "name":             "large_powerwall"
-  ##################################################
-    , "hostname":         "kerberos"
-    , "displaystrings":   [":0.0", ":0.1", ":0.2", ":0.3"]
-    , "resolution":       (1920, 1200)
-    , "size":             (4.16, 2.6)
-    , "transform":        (0.0, 1.57, 0.0)
-    , "warpmatrices":     [
-      (
-          "/opt/dlp-warpmatrices/dlp_6_warp_P4.warp"
-        , "/opt/dlp-warpmatrices/dlp_6_warp_P5.warp"
-        , "/opt/dlp-warpmatrices/dlp_6_warp_P6.warp"
-        , "/opt/dlp-warpmatrices/dlp_6_warp_P1.warp"
-        , "/opt/dlp-warpmatrices/dlp_6_warp_P2.warp"
-        , "/opt/dlp-warpmatrices/dlp_6_warp_P3.warp"
-      )
-      , (
-          "/opt/dlp-warpmatrices/dlp_6_warp_P4.warp"
-        , "/opt/dlp-warpmatrices/dlp_6_warp_P5.warp"
-        , "/opt/dlp-warpmatrices/dlp_6_warp_P6.warp"
-        , "/opt/dlp-warpmatrices/dlp_6_warp_P1.warp"
-        , "/opt/dlp-warpmatrices/dlp_6_warp_P2.warp"
-        , "/opt/dlp-warpmatrices/dlp_6_warp_P3.warp"
-      )
-      , (
-          "/opt/dlp-warpmatrices/dlp_6_warp_P4.warp"
-        , "/opt/dlp-warpmatrices/dlp_6_warp_P5.warp"
-        , "/opt/dlp-warpmatrices/dlp_6_warp_P6.warp"
-        , "/opt/dlp-warpmatrices/dlp_6_warp_P1.warp"
-        , "/opt/dlp-warpmatrices/dlp_6_warp_P2.warp"
-        , "/opt/dlp-warpmatrices/dlp_6_warp_P3.warp"
-      )
-      , (
-          "/opt/dlp-warpmatrices/dlp_6_warp_P4.warp"
-        , "/opt/dlp-warpmatrices/dlp_6_warp_P5.warp"
-        , "/opt/dlp-warpmatrices/dlp_6_warp_P6.warp"
-        , "/opt/dlp-warpmatrices/dlp_6_warp_P1.warp"
-        , "/opt/dlp-warpmatrices/dlp_6_warp_P2.warp"
-        , "/opt/dlp-warpmatrices/dlp_6_warp_P3.warp"
-      )
-    ]
-  }
+from Display import Display
 
-  , {
-  ##################################################
-      "name":             "small_powerwall"
-  ##################################################
-    , "hostname":         "tba"
-    , "displaystrings":   [":0.0", ":0.1"]
-    , "resolution":       (1920, 1200)
-    , "size":             (3.0, 1.98)
-    , "transform":        (0.0, 1.42, 0.0)
-    , "warpmatrices":     [
-      (
-          "/opt/lcd-warpmatrices/lcd_4_warp_P2.warp"
-        , "/opt/lcd-warpmatrices/lcd_4_warp_P2.warp"
-        , "/opt/lcd-warpmatrices/lcd_4_warp_P2.warp"
-        , "/opt/lcd-warpmatrices/lcd_4_warp_P1.warp"
-        , "/opt/lcd-warpmatrices/lcd_4_warp_P1.warp"
-        , "/opt/lcd-warpmatrices/lcd_4_warp_P1.warp"
-      )
-      , (
-          "/opt/lcd-warpmatrices/lcd_4_warp_P4.warp"
-        , "/opt/lcd-warpmatrices/lcd_4_warp_P4.warp"
-        , "/opt/lcd-warpmatrices/lcd_4_warp_P4.warp"
-        , "/opt/lcd-warpmatrices/lcd_4_warp_P3.warp"
-        , "/opt/lcd-warpmatrices/lcd_4_warp_P3.warp"
-        , "/opt/lcd-warpmatrices/lcd_4_warp_P3.warp"
-      )
-    ]
-  }
+##################################################
+class LargePowerwall(Display):
+##################################################
+  def __init__(self):
+    self.name = "large_powerwall"
+    self.hostname = "kerberos"
+    self.resolution = (1920, 1200)
+    self.displaystrings = [":0.0", ":0.1", ":0.2", ":0.3"]
+    self.size = (4.16, 2.6)
+    self.transform = (0.0, 1.57, 0.0)
 
-  , {
-  ##################################################
-      "name":             "atalante_display"
-  ##################################################
-    , "hostname":         "atalante"
-    , "resolution":       (1920, 1080)
-    , "size":             (0.40, 0.20)
-    , "transform":        (0.0, 1.0, 0.0)
-  }
+  def register_user(self, user_num):
+    if user_num < 4:
+      warpmatrices = [
+          "/opt/dlp-warpmatrices/dlp_6_warp_P4.warp"
+        , "/opt/dlp-warpmatrices/dlp_6_warp_P5.warp"
+        , "/opt/dlp-warpmatrices/dlp_6_warp_P6.warp"
+        , "/opt/dlp-warpmatrices/dlp_6_warp_P1.warp"
+        , "/opt/dlp-warpmatrices/dlp_6_warp_P2.warp"
+        , "/opt/dlp-warpmatrices/dlp_6_warp_P3.warp"
+      ]
+      return (self.displaystrings[user_num], warpmatrices)
+    else:
+      return None
+
+##################################################
+class SmallPowerwall(Display):
+##################################################
+  def __init__(self):
+    self.name = "large_powerwall"
+    self.hostname = "tba"
+    self.resolution = (1920, 1200)
+    self.displaystrings = [":0.0", ":0.1"]
+    self.size = (3.0, 1.98)
+    self.transform = (0.0, 1.42, 0.0)
+
+  def register_user(self, user_num):
+    if user_num < 2:
+      warpmatrices = [
+          "{0}lcd_4_warp_P{1}.warp".format(warp_matrices_path, 2 * user_num + 2)
+        , "{0}lcd_4_warp_P{1}.warp".format(warp_matrices_path, 2 * user_num + 2)
+        , "{0}lcd_4_warp_P{1}.warp".format(warp_matrices_path, 2 * user_num + 2)
+      
+        , "{0}lcd_4_warp_P{1}.warp".format(warp_matrices_path, 2 * user_num + 1)
+        , "{0}lcd_4_warp_P{1}.warp".format(warp_matrices_path, 2 * user_num + 1)
+        , "{0}lcd_4_warp_P{1}.warp".format(warp_matrices_path, 2 * user_num + 1)
+      ]
+      return (self.displaystrings[user_num], warpmatrices)
+    else:
+      return None
+
+##################################################
+# STORE ALL DISPLAYS TO BE USED IN THIS LIST
+##################################################
+displays = [
+    LargePowerwall()
+  , SmallPowerwall()
+  , Display("atalante")
 ]
--- lib-server/Display.py
+++ lib-server/Display.py
@@ -4,36 +4,29 @@
 import avango
 import avango.gua
 
-from display_config import display_config
-
-def create_displays():
-	displays = [ ]
-	for config in display_config:
-		if ("name" and "hostname") in config:
-			displays.append(Display(config))
-	return displays
-
-
 class Display:
 
-	DEFAULT_DISPLAYSTRINGS 	= [":0.0"]
-	DEFAULT_RESOLUTION			= (800, 600)
-	DEFAULT_SIZE						= (1.6, 0.9)
-	DEFAULT_TRANSFORM				= (0.0, 0.0, 0.0)
+	def __init__(	self
+							, hostname
+							, name = None
+							, resolution = (1920, 1080)
+							, displaystrings = [":0.0"]
+							, size = (1.6, 0.9)
+							, transform = (0, 1.0, 0)
+							):
+		# save values in members
+		self.hostname = hostname
+		if not name:
+			self.name = hostname + "_display"
+		else:
+			self.name = name
+		self.resolution = resolution
+		self.displaystrings = displaystrings
+		self.size = size
+		self.transform = transform
 
-	def __init__(self, config):
-		# get data from config-dict
-		self.name = config.get("name")
-		self.hostname = config.get("hostname")
-		self.displaystrings = config.get("displaystrings", self.DEFAULT_DISPLAYSTRINGS)
-		_width, _height = config.get("size", self.DEFAULT_SIZE)
-		_trans_x, _trans_y, _trans_z = config.get("transform", self.DEFAULT_TRANSFORM)
-
-		# init counter
-		self.num_users = 0
-
-	def register_user(self):
-
-		# TODO return values needed by client (displaystring, warpmatrices)
-
-		pass
+	def register_user(self, user_num):
+		if user_num < len(self.displaystrings):
+			return self.displaystrings[user_num]
+		else:
+			return None

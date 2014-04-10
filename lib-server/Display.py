2073bbea78151242ee485ccf939e338ebd1cfd4d
Joshua Reibert <joshua.reibert@uni-weimar.de>
2014-04-10 13:23:32 +0200

    fix setting resolution as screen size

--- lib-server/Display.py
+++ lib-server/Display.py
@@ -12,7 +12,7 @@ class Display:
 							, resolution = (1920, 1080)
 							, displaystrings = [":0.0"]
 							, size = (1.6, 0.9)
-							, transform = (0.0, 1.2, 0.0)
+							, transform = (0.0, 1.75 / 2.0, 0.0)
 							):
 		# save values in members
 		self.hostname = hostname
@@ -37,7 +37,7 @@ class Display:
 
 	def create_screen_node(self, name = "screen_node"):
 		_screen = avango.gua.nodes.ScreenNode(Name = name)
-		_w, _h = self.resolution
+		_w, _h = self.size
 		_screen.Width.value = _w
 		_screen.Height.value = _h
 		_x, _y, _z = self.transform

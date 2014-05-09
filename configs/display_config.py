1f6b845b5a8b1831e75bc5d880cdc8eb79c863b9
Tim Weissker <tim.weissker@uni-weimar.de>
2014-05-09 19:08:09 +0200

    Add daedalos desktop setup.

--- configs/display_config.py
+++ configs/display_config.py
@@ -169,6 +169,10 @@ displays = [
       , transformation = avango.gua.make_trans_mat(0.0, 1.2, 0.0)
       #, transformation = avango.gua.make_trans_mat(0.645, 1.2, 0.0)
   )
+  , Display(hostname = "daedalos"
+      , transformation = avango.gua.make_trans_mat(0.0, 1.2, 0.0)
+      #, transformation = avango.gua.make_trans_mat(0.645, 1.2, 0.0)
+  )
   , Display(hostname = "nestor"
       , transformation = avango.gua.make_trans_mat(0.0, 1.2, 0.0)
   )

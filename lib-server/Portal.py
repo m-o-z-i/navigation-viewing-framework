406b473ab627f158c58829e87d831ce5803fcf4f
Tim Weissker <tim.weissker@uni-weimar.de>
2014-06-17 13:35:39 +0200

    Use frame counter to not teleport for 100 frames after last teleport.

--- lib-server/Portal.py
+++ lib-server/Portal.py
@@ -32,6 +32,10 @@ class PortalManager(avango.script.Script):
     # List of all Navigation instances checked for portal updates.
     self.NAVIGATION_LIST = NAVIGATION_LIST
 
+    ## @var teleportation_frame_list
+    # List containing the frames left until the next teleportation is possible for a Navigation.
+    self.teleportation_frame_list = [0 for i in range(len(self.NAVIGATION_LIST))]
+
     ## @var portal_group_node
     # Scenegraph grouping node for portals on server side.
     self.portal_group_node = avango.gua.nodes.TransformNode(Name = "portal_group")
@@ -46,40 +50,40 @@ class PortalManager(avango.script.Script):
     self.counter = 0
 
     # add portal instances
-    self.add_portal(avango.gua.make_trans_mat(0.0, 1.55, 0.0) * avango.gua.make_rot_mat(-90, 0, 1, 0),
-                    avango.gua.make_trans_mat(0.0, 2.0, -1.5) * avango.gua.make_rot_mat(45, 1, 0, 0),
-                    1.0,
-                    1.0,
-                    "3D",
-                    "PERSPECTIVE",
-                    "False",
-                    "data/materials/ShadelessBlue.gmd")
+    #self.add_portal(avango.gua.make_trans_mat(0.0, 1.55, 0.0) * avango.gua.make_rot_mat(-90, 0, 1, 0),
+    #                avango.gua.make_trans_mat(0.0, 2.0, -1.5) * avango.gua.make_rot_mat(45, 1, 0, 0),
+    #                1.0,
+    #                1.0,
+    #                "3D",
+    #                "PERSPECTIVE",
+    #                "False",
+    #                "data/materials/ShadelessBlue.gmd")
 
-    self.add_portal(avango.gua.make_trans_mat(0.0, 1.2, 1.0), 
-                    avango.gua.make_trans_mat(-1.2, 2.0, -2.5),
-                    1.0,
-                    1.0,
-                    "3D",
-                    "PERSPECTIVE",
-                    "False",
-                    "data/materials/ShadelessBlue.gmd")
+    #self.add_portal(avango.gua.make_trans_mat(0.0, 1.2, 1.0), 
+    #                avango.gua.make_trans_mat(-1.2, 2.0, -2.5),
+    #                1.0,
+    #                1.0,
+    #                "3D",
+    #                "PERSPECTIVE",
+    #                "False",
+    #                "data/materials/ShadelessBlue.gmd")
 
-    self.add_portal(avango.gua.make_trans_mat(0.0, 1.55, 0.0) * avango.gua.make_rot_mat(90, 0, 1, 0),
-                    avango.gua.make_trans_mat(1.2, 2.0, -2.5),
-                    1.0,
-                    1.0,
-                    "3D",
-                    "PERSPECTIVE",
-                    "False",
-                    "data/materials/ShadelessBlue.gmd")
+    #self.add_portal(avango.gua.make_trans_mat(0.0, 1.55, 0.0) * avango.gua.make_rot_mat(90, 0, 1, 0),
+    #                avango.gua.make_trans_mat(1.2, 2.0, -2.5),
+    #                1.0,
+    #                1.0,
+    #                "3D",
+    #                "PERSPECTIVE",
+    #                "False",
+    #                "data/materials/ShadelessBlue.gmd")
 
-    #self.add_bidirectional_portal(avango.gua.make_trans_mat(0.0, 1.55, 3.0),
-    #                              avango.gua.make_trans_mat(0.0, 1.55, -3.0),
-    #                              1.0,
-    #                              1.0,
-    #                              "3D",
-    #                              "PERSPECTIVE",
-    #                              "False")
+    self.add_bidirectional_portal(avango.gua.make_trans_mat(0.0, 1.55, 3.0),
+                                  avango.gua.make_trans_mat(0.0, 1.55, -3.0),
+                                  1.0,
+                                  1.0,
+                                  "3D",
+                                  "PERSPECTIVE",
+                                  "False")
 
     self.always_evaluate(True)
 
@@ -89,6 +93,8 @@ class PortalManager(avango.script.Script):
     for _nav in self.NAVIGATION_LIST:
 
       _mat = _nav.platform.platform_transform_node.WorldTransform.value * _nav.device.sf_station_mat.value
+      _frame_count = self.teleportation_frame_list[self.NAVIGATION_LIST.index(_nav)]
+      print _frame_count
 
       for _portal in self.portals:
 
@@ -97,16 +103,24 @@ class PortalManager(avango.script.Script):
 
         _vec_in_portal_space = _mat_in_portal_space.get_translate()
 
-        if (_vec_in_portal_space.x > -_portal.width/2 and \
-            _vec_in_portal_space.x <  _portal.width/2 and \
+        if (_vec_in_portal_space.x > -_portal.width/2  and \
+            _vec_in_portal_space.x <  _portal.width/2  and \
             _vec_in_portal_space.y > -_portal.height/2 and \
             _vec_in_portal_space.y <  _portal.height/2 and \
-            abs(_vec_in_portal_space.z) < 0.1):
+            abs(_vec_in_portal_space.z) < 0.1)         and \
+            _frame_count <= 0:
 
           if _portal.viewing_mode == "3D":
             _nav.inputmapping.set_abs_mat(_portal.scene_matrix * avango.gua.make_trans_mat(_vec_in_portal_space) * avango.gua.make_rot_mat(_mat_in_portal_space.get_rotate()) * avango.gua.make_inverse_mat(_nav.device.sf_station_mat.value) )
           else:
             _nav.inputmapping.set_abs_mat(_portal.scene_matrix * avango.gua.make_trans_mat(_vec_in_portal_space) * avango.gua.make_inverse_mat(_nav.device.sf_station_mat.value) )
+
+          self.teleportation_frame_list[self.NAVIGATION_LIST.index(_nav)] = 100
+        
+        else:
+          
+          if _frame_count > 0:
+            self.teleportation_frame_list[self.NAVIGATION_LIST.index(_nav)] = _frame_count - 1
       
 
 

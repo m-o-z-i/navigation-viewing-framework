c1f8118d220aa8e1dd9a0f0f127b37cd429a447f
Tim Weissker <tim.weissker@uni-weimar.de>
2014-09-04 10:24:27 +0200

    Add configuration files and remove them from gitignore.

---
+++ configs/atalante_desktop.py
@@ -0,0 +1,66 @@
+#!/usr/bin/python
+
+## @file
+# Contains workspace, display, navigation, display group and user configuration classes to be used by the framework.
+
+# import guacamole libraries
+import avango
+import avango.gua
+
+# import framework libraries
+from Display import *
+from Workspace import Workspace
+from SteeringNavigation import SteeringNavigation
+from StaticNavigation import StaticNavigation
+
+## Create Workspaces first ##
+atalante = Workspace('atalante', avango.gua.make_trans_mat(0.0, 0.043, 0.0))
+
+workspaces = [atalante]
+
+## Create Navigation instances ##
+spacemouse_navigation = SteeringNavigation()
+spacemouse_navigation.my_constructor( STARTING_MATRIX = avango.gua.make_trans_mat(0, 0, 0)
+                                    , STARTING_SCALE = 1.0
+                                    , INPUT_DEVICE_TYPE = 'Spacemouse'
+                                    , INPUT_DEVICE_NAME = 'device-spacemouse'
+                                    , NO_TRACKING_MAT = avango.gua.make_trans_mat(0.0, 1.2, 0.6)
+                                    , GROUND_FOLLOWING_SETTINGS = [True, 0.75]
+                                    , MOVEMENT_TRACES = True
+                                    , INVERT = False
+                                    , AVATAR_TYPE = 'joseph'
+                                    , DEVICE_TRACKING_NAME = None)
+
+keyboard_navigation = SteeringNavigation()
+keyboard_navigation.my_constructor( STARTING_MATRIX = avango.gua.make_trans_mat(0, 0, 0)
+                                    , STARTING_SCALE = 1.0
+                                    , INPUT_DEVICE_TYPE = 'KeyboardMouse'
+                                    , INPUT_DEVICE_NAME = None
+                                    , NO_TRACKING_MAT = avango.gua.make_trans_mat(0.0, 1.2, 0.6)
+                                    , GROUND_FOLLOWING_SETTINGS = [True, 0.75]
+                                    , MOVEMENT_TRACES = True
+                                    , INVERT = False
+                                    , AVATAR_TYPE = 'joseph'
+                                    , DEVICE_TRACKING_NAME = None)
+
+static_navigation = StaticNavigation()
+static_navigation.my_constructor(STATIC_ABS_MAT = avango.gua.make_trans_mat(0, 10, 10)
+                               , STATIC_SCALE = 1.0
+                               , AVATAR_TYPE = 'joseph')
+
+## Create Display instances. ##
+atalante_desktop_display = Display(hostname = "atalante", transformation = avango.gua.make_trans_mat(0.0, 1.2, 0.0))
+
+displays = [atalante_desktop_display]
+
+## Create display groups ##
+atalante.create_display_group( DISPLAY_LIST = [atalante_desktop_display]
+                             , NAVIGATION_LIST = [spacemouse_navigation, keyboard_navigation, static_navigation]
+                             , OFFSET_TO_WORKSPACE = avango.gua.make_trans_mat(0, 0, 0) )
+
+## Create users ##
+atalante.create_user(  VIP = False
+                    , GLASSES_ID = None
+                    , HEADTRACKING_TARGET_NAME = None
+                    , EYE_DISTANCE = 0.065
+                    , NO_TRACKING_MAT = avango.gua.make_trans_mat(0.0, 1.2, 0.6))
---
+++ configs/scene_config.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python
+
+## @file
+# Contains the scenegraph instances and scene names to be used by the framework.
+
+# import guacamole libraries
+import avango
+import avango.gua
+
+# import python libraries
+import subprocess
+
+# get server ip 
+server_ip = subprocess.Popen(["hostname", "-I"], stdout=subprocess.PIPE).communicate()[0]
+server_ip = server_ip.strip(" \n")  
+server_ip = server_ip.rsplit(" ")
+server_ip = str(server_ip[-1])
+
+# create raw scenegraph
+graph = avango.gua.nodes.SceneGraph(Name = "scenegraph")
+
+nettrans = avango.gua.nodes.NetTransform( Name = "net"
+                                        , Groupname = "AVSERVER|{0}|7432".format(server_ip))
+graph.Root.value.Children.value.append(nettrans)
+
+# create scenegraph list
+scenegraphs = [graph]
+
+# create scene list, strings must match a class name in Scene.py
+scenes = ["SceneMedievalTown"]
+#scenes = ["SceneValcamonicaOptimized"]
+
+# if true, scenes will be switchable using the number buttons
+enable_key_bindings = True
+
+## alternative scenes:
+# SceneWeimar
+# SceneMonkey
+# SceneMedievalTown
+# ScenePitoti
+# SceneVianden
+# SceneViandenLow
+# SceneViandenHigh
+# ScenePLOD
